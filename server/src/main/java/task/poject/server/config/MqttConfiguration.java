0a1a6ec41e83ee098d4591bfdb266ef99e197c5e
min-0 <dhe08133@naver.com>
2023-07-10 14:45:49 +0900

    chore: Add Config

---
+++ server/src/main/java/task/poject/server/config/MqttConfiguration.java
@@ -0,0 +1,11 @@
+package task.poject.server.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MqttConfiguration {
+
+    // ToDo
+    // mqtt broker, id, topic 설정
+
+}
